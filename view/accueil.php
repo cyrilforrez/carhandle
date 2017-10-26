740f86dacbc034334a1a92830b04f029b351c75f
cyrilforrez <cyilforrez@gmail.com>
2017-10-26 18:03:01 +0200

    modif navbar replace btn detail for btn add

--- view/accueil.php
+++ view/accueil.php
@@ -36,48 +36,6 @@ require ('includes/header.php');
       </table>
 
 <div id="divboutton">
-  <a class="waves-effect waves-light btn deep-orange darken-3">Voir</a>
-  <a class="waves-effect waves-light btn modal-trigger deep-orange darken-3" href="#modal1">Ajouter</a>
-  <!-- Modal Structure -->
-  <div id="modal1" class="modal">
-    <div class="modal-content">
-      <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="camion, voiture, moto" id="type" type="text" class="validate">
-          <label for="type">Type</label>
-        </div>
-        <div class="input-field col s12">
-          <input placeholder="scania, audi, ducati" id="marque" type="text" class="validate">
-          <label for="marque">Marque</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="A3, 899" id="modele" type="text" class="validate">
-          <label for="modele">Modele</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="20 000" id="prix" type="text" class="validate">
-          <label for="prix">Pirx</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <textarea placeholder="Etat du véhicule etc..." id="description" class="materialize-textarea"></textarea>
-          <label for="description">Description</label>
-        </div>
-      </div>
-    </form>
-  </div>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-  </div>
   <a class="waves-effect waves-light btn deep-orange darken-3">supprimer</a>
 </div>
 
--- view/includes/header.php
+++ view/includes/header.php
@@ -6,12 +6,93 @@
      <div class="nav-wrapper grey darken-3">
        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
        <ul class="left hide-on-med-and-down" id="nav">
-         <li class="deep-orange darken-3" id="home"><a href="accueil.php">Home</a></li>
-         <li class="deep-orange darken-3" id="detail"><a href="detail.php">Detail</a></li>
+         <li class="deep-orange darken-3" id="home"><a href="controler/accueil.php">Home</a></li>
+         <li class="deep-orange darken-3" id="ajout"><a class="modal-trigger deep-orange darken-3" href="#modal1">Ajouter</a>
+         <!-- Modal Structure -->
+         <div id="modal1" class="modal">
+           <div class="modal-content">
+             <div class="row">
+           <form class="col s12">
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="camion, voiture, moto" id="type" type="text" class="validate">
+                 <label for="type">Type</label>
+               </div>
+               <div class="input-field col s12">
+                 <input placeholder="scania, audi, ducati" id="marque" type="text" class="validate">
+                 <label for="marque">Marque</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="A3, 899" id="modele" type="text" class="validate">
+                 <label for="modele">Modele</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="20 000" id="prix" type="text" class="validate">
+                 <label for="prix">Pirx</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <textarea placeholder="Etat du véhicule etc..." id="description" class="materialize-textarea"></textarea>
+                 <label for="description">Description</label>
+               </div>
+             </div>
+           </form>
+         </div>
+           </div>
+           <div class="modal-footer">
+             <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+           </div>
+         </div></li>
        </ul>
+       <!--################ Menu burger ########################-->
        <ul class="side-nav" id="mobile-demo">
          <li class="deep-orange darken-3"><a href="accueil.php">Home</a></li>
-         <li class="deep-orange darken-3"><a href="detail.php">Detail</a></li>
+         <li class="deep-orange darken-3"><a class="modal-trigger deep-orange darken-3" href="#modal1">Ajouter</a>
+         <!-- Modal Structure -->
+         <div id="modal1" class="modal">
+           <div class="modal-content">
+             <div class="row">
+           <form class="col s12">
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="camion, voiture, moto" id="type" type="text" class="validate">
+                 <label for="type">Type</label>
+               </div>
+               <div class="input-field col s12">
+                 <input placeholder="scania, audi, ducati" id="marque" type="text" class="validate">
+                 <label for="marque">Marque</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="A3, 899" id="modele" type="text" class="validate">
+                 <label for="modele">Modele</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <input placeholder="20 000" id="prix" type="text" class="validate">
+                 <label for="prix">Pirx</label>
+               </div>
+             </div>
+             <div class="row">
+               <div class="input-field col s12">
+                 <textarea placeholder="Etat du véhicule etc..." id="description" class="materialize-textarea"></textarea>
+                 <label for="description">Description</label>
+               </div>
+             </div>
+           </form>
+         </div>
+           </div>
+           <div class="modal-footer">
+             <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+           </div>
+         </div></li>
        </ul>
      </div>
    </nav>
