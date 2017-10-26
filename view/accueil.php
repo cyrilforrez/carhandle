575a7a032e9df01bd3aca992a0452ad7a734e8b8
cyrilforrez <cyilforrez@gmail.com>
2017-10-26 16:35:12 +0200

    setting up the css

--- view/accueil.php
+++ view/accueil.php
@@ -3,7 +3,7 @@ require ('includes/head.php');
 require ('includes/header.php');
  ?>
 
-    <table class="highlight bordered responsive-table" >
+    <table class="highlight bordered responsive-table" id="tableau" >
         <thead>
           <tr>
               <th>Type</th>
@@ -15,40 +15,71 @@ require ('includes/header.php');
 
         <tbody>
           <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
+            <td>camion</td>
+            <td>scania</td>
+            <td>...</td>
+            <td>40,000e</td>
           </tr>
           <tr>
             <td>Alan</td>
             <td>Jellybean</td>
+            <td></td>
             <td>$3.76</td>
           </tr>
           <tr>
             <td>Jonathan</td>
             <td>Lollipop</td>
+            <td></td>
             <td>$7.00</td>
           </tr>
         </tbody>
       </table>
 
-
-<a class="waves-effect waves-light btn">Voir</a>
-
-<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Ajouter</a>
-
+<div id="divboutton">
+  <a class="waves-effect waves-light btn deep-orange darken-3">Voir</a>
+  <a class="waves-effect waves-light btn modal-trigger deep-orange darken-3" href="#modal1">Ajouter</a>
   <!-- Modal Structure -->
   <div id="modal1" class="modal">
     <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
+      <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="camion, voiture, moto" id="type" type="text" class="validate">
+          <label for="type">Type</label>
+        </div>
+        <div class="input-field col s12">
+          <input placeholder="scania, audi, ducati" id="marque" type="text" class="validate">
+          <label for="marque">Marque</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="A3, 899" id="modele" type="text" class="validate">
+          <label for="modele">Modele</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="20 000" id="prix" type="text" class="validate">
+          <label for="prix">Pirx</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea placeholder="Etat du véhicule etc..." id="description" class="materialize-textarea"></textarea>
+          <label for="description">Description</label>
+        </div>
+      </div>
+    </form>
+  </div>
     </div>
     <div class="modal-footer">
       <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
     </div>
   </div>
-
-<a class="waves-effect waves-light btn">supprimer</a>
+  <a class="waves-effect waves-light btn deep-orange darken-3">supprimer</a>
+</div>
 
 
 <?php
--- view/includes/header.php
+++ view/includes/header.php
@@ -5,13 +5,13 @@
   <nav>
      <div class="nav-wrapper grey darken-3">
        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-       <ul class="left hide-on-med-and-down">
-         <li><a href="accueil.php">Home</a></li>
-         <li><a href="detail.php">Detail</a></li>
+       <ul class="left hide-on-med-and-down" id="nav">
+         <li class="deep-orange darken-3" id="home"><a href="accueil.php">Home</a></li>
+         <li class="deep-orange darken-3" id="detail"><a href="detail.php">Detail</a></li>
        </ul>
        <ul class="side-nav" id="mobile-demo">
-         <li><a href="accueil.php">Home</a></li>
-         <li><a href="detail.php">Detail</a></li>
+         <li class="deep-orange darken-3"><a href="accueil.php">Home</a></li>
+         <li class="deep-orange darken-3"><a href="detail.php">Detail</a></li>
        </ul>
      </div>
    </nav>
